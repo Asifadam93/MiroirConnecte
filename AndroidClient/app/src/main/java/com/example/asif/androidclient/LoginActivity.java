2253645f2e86cc7f90f7d02d8e57c712a13e7d04
AAD <a.adamsha@magneta.fr>
2017-06-12 16:47:12 +0200

    edits

--- AndroidClient/app/src/main/java/com/example/asif/androidclient/LoginActivity.java
+++ AndroidClient/app/src/main/java/com/example/asif/androidclient/LoginActivity.java
@@ -2,12 +2,49 @@ package com.example.asif.androidclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button buttonLogIn, buttonInscription;
+    private EditText editTextNom, editTextMdp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonLogIn = (Button)findViewById(R.id.buttonLogin);
+        buttonInscription = (Button)findViewById(R.id.buttonInscription);
+
+        editTextNom = (EditText)findViewById(R.id.editTextNom);
+        editTextMdp = (EditText)findViewById(R.id.editTextMDP);
+
+        buttonLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String nom = editTextNom.getText().toString();
+                String mdp = editTextMdp.getText().toString();
+
+                userAuthentification(nom,mdp);
+            }
+        });
+
+        buttonInscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+    }
+
+    private void userAuthentification(String nom, String mdp){
+
     }
 }
