637332607aac63e7a90831dc07e6eead00a0719e
fisamada39 <asifadam93@hotmail.fr>
2017-07-02 20:23:02 +0200

    AndroidClient version 2 initial commit

---
+++ AndroidClientV2/app/src/main/java/com/esgi/androidclientv2/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.esgi.androidclientv2.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.esgi.androidclientv2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
