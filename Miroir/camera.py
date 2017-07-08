205e0c1a29013e8aebe79df555f18e6e43a96317
PetitGrigri <fgriselles@hotmail.fr>
2017-07-08 12:04:27 +0200

    Update prise de la photo

--- Miroir/camera.py
+++ Miroir/camera.py
@@ -1,11 +1,12 @@
 from picamera import PiCamera
 
 camera = PiCamera()
-
+camera.hflip = True
+camera.vflip = True
+camera.resolution = (320, 240)
 
 def take_picture():
     global camera
-    camera.resolution = (320, 240)
-    camera.start_preview()
+    camera.start_preview(fullscreen=False, window = (100, 20, 640, 480))
     camera.capture("img/tempo_photo/photo.jpg")
     camera.stop_preview()
