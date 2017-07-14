63e825eb5101ffe2d2a30ef7c5059fb19268b688
PetitGrigri <fgriselles@hotmail.fr>
2017-07-14 11:03:46 +0200

    update miroir prise de phoyo et update

--- Miroir/main.py
+++ Miroir/main.py
@@ -21,28 +21,12 @@ output = np.empty((240, 320, 3), dtype=np.uint8)
 # utilisateur reconnu
 actual_user = 0
 
+# Prise d'une photo, ou mise a jour en cours
+photo_or_update_in_progress = False
+
 # Définition des boutons qu'on utilisera
 button_1 = Button(17)
 button_2 = Button(27)
-
-
-#
-def button_1_pressed():
-    camera.start_preview(int((affichage.fenetre_center_left )-320), int((affichage.fenetre_block_height +60)))
-
-    compteur = 5
-    while compteur >= 0:
-        affichage.widget_information.config(text="Prise d'une photo dans " + str(compteur) + " seconde(s)")
-        compteur -= 1
-        time.sleep(1)
-    chemin_photo = camera.take_picture()
-    affichage.widget_information.config(text=str("Photo prise, envoie de la photo au serveur"))
-    code_photo = apiMirror.post_photo(chemin_photo)
-    affichage.widget_information.config(text="Votre code photo : "+str(code_photo))
-
-#
-def button_2_pressed():
-    affichage.widget_information.config(text="Bouton 2")
 
 
 #
@@ -50,14 +34,26 @@ def add_user(photo_name, id):
     global known_faces, known_users
 
     if isfile("./img/photos/" + photo_name):
-        print("Ajout de " + str(id) + " " + photo_name)
-        image = face_recognition.load_image_file("./img/photos/" + user["photo_name"])
-        image_encoding = face_recognition.face_encodings(image)[0]
-        known_faces.append(image_encoding)
-        known_users.append(id)
+        try :
+            already_add = known_users.index(id)
+            print("L'utilisateur : " + str(id) + " est déjà présent")
+        except :
+            print("Ajout de " + str(id) + " " + photo_name)
+            image = face_recognition.load_image_file("./img/photos/" + photo_name)
+            image_encoding = face_recognition.face_encodings(image)[0]
+            known_faces.append(image_encoding)
+            known_users.append(id)
     else:
         print("Erreur avec " + str(id) + " " + photo_name)
 
+def update_faces() :
+    # Récupération de tout les utilisateurs (et téléchargement de leurs photos si elles ne sont pas déjà présentes)
+    users = apiMirror.get_users()
+
+    pprint(users)
+
+    for index, user in enumerate(users):
+        add_user(user["photo_name"], user["id"])
 
 def update_mirror(user_id):
     global actual_user
@@ -89,41 +85,67 @@ def update_mirror(user_id):
 
 #
 def search_face():
-    global output, known_faces, known_users, face_encodings, face_locations
+    global output, known_faces, known_users, face_encodings, face_locations, photo_in_progress
 
-    print("Je recherche un visage")
-    camera.camera.capture(output, format="rgb")
+    if not photo_or_update_in_progress:
+        print("Je recherche un visage")
+        camera.camera.capture(output, format="rgb")
 
-    print("Found {} faces in image.".format(len(face_locations)))
-    face_locations = face_recognition.face_locations(output)
+        print("Found {} faces in image.".format(len(face_locations)))
+        face_locations = face_recognition.face_locations(output)
 
-    faces_encoding = face_recognition.face_encodings(output, face_locations)
+        faces_encoding = face_recognition.face_encodings(output, face_locations)
 
-    # Loop over each face found in the frame to see if it's someone we know.
-    for face_encoding in faces_encoding:
-        # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_faces, face_encoding)
+        # Loop over each face found in the frame to see if it's someone we know.
+        for face_encoding in faces_encoding:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_faces, face_encoding)
 
-        for index, match in (enumerate(matches)):
-            if match == True:
-                print("Trouvé")
-                update_mirror(known_users[index])
+            for index, match in (enumerate(matches)):
+                if match == True:
+                    print("Trouvé : ", known_users[index])
+                    update_mirror(known_users[index])
+    else:
+        print("Pas de recherche à faire pour le moment")
 
     t = Timer(1, search_face)
     t.start()
 
 
+def button_1_pressed():
+    global photo_or_update_in_progress
+    photo_or_update_in_progress = True
+    print("Je vais prendre une photo")
+    camera.start_preview(int((affichage.fenetre_center_left )-320), int((affichage.fenetre_block_height +60)))
+
+    compteur = 5
+    while compteur >= 0:
+        affichage.widget_information.config(text="Prise d'une photo dans " + str(compteur) + " seconde(s)")
+        compteur -= 1
+        time.sleep(1)
+    print("Prise de la photo")
+    chemin_photo = camera.take_picture()
+    affichage.widget_information.config(text=str("Photo prise, envoie de la photo au serveur"))
+    code_photo = apiMirror.post_photo(chemin_photo)
+    affichage.widget_information.config(text="Votre code photo : "+str(code_photo))
+    photo_or_update_in_progress = False
+#
+def button_2_pressed():
+    global actual_user, photo_or_update_in_progress
+    photo_or_update_in_progress = True
+    actual_user = 0
+    affichage.widget_information.config(text="")
+    affichage.clear_mirror()
+    update_faces()
+    photo_or_update_in_progress = False
+
 # Ajout des comportements qux boutons
 button_1.when_pressed = button_1_pressed
 button_2.when_pressed = button_2_pressed
 
-# Récupération de tout les utilisateurs (et téléchargement de leurs photos si elles ne sont pas déjà présentes)
-users = apiMirror.get_users()
+# Recuperation de la liste de tout les utilisateurs
+update_faces()
 
-pprint(users)
-
-for index,user in enumerate(users):
-    add_user(user["photo_name"], user["id"])
 
 t = Timer(1, search_face)
 t.start()
